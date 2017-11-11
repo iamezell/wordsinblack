a4d3a762366d93276f484e0c5b83dce823c67986
Ezell <iamezell@gmail.com>
2017-11-11 12:50:15 -0500

    fix: directory structure

---
+++ Assets/WordGame/Scripts/UI/UIScreenCompleteOverlay.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIScreenCompleteOverlay : UIScreen
+{
+	#region Inspector Variables
+
+	[SerializeField] private Image		categoryIconImage;
+	[SerializeField] private Text		categoryNameText;
+	[SerializeField] private Text		categoryLevelText;
+	[SerializeField] private GameObject	plusOneHintText;
+
+	#endregion
+
+	#region Member Variables
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region Unity Methods
+	#endregion
+
+	#region Public Methods
+
+	public override void OnShowing(object data)
+	{
+		CategoryInfo categoryInfo = GameManager.Instance.GetCategoryInfo(GameManager.Instance.ActiveCategory);
+
+		categoryIconImage.sprite	= categoryInfo.icon;
+		categoryNameText.text		= GameManager.Instance.ActiveCategory;
+
+		if (GameManager.Instance.ActiveCategory == GameManager.dailyPuzzleId)
+		{
+			categoryLevelText.gameObject.SetActive(false);
+		}
+		else
+		{
+			categoryLevelText.gameObject.SetActive(true);
+			categoryLevelText.text = "Level " + (GameManager.Instance.ActiveLevelIndex + 1).ToString();
+		}
+
+		plusOneHintText.SetActive((bool)data);
+	}
+
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
