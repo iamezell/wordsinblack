dc85ee97c06d378b5361728b16614d4c4e74fcf8
Ezell <iamezell@gmail.com>
2017-11-11 12:32:06 -0500

    adding base

---
+++ Youdon'tknowblack/Assets/WordGame/Scripts/UI/UIScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIScreen : MonoBehaviour
+{
+	#region Inspector Variables
+
+	public string			id;
+	public List<GameObject>	worldObjects;
+
+	#endregion
+
+	#region Properties
+
+	public RectTransform RectT { get { return gameObject.GetComponent<RectTransform>(); } }
+
+	#endregion
+
+	#region Public Methods
+
+	public virtual void Initialize()
+	{
+
+	}
+
+	public virtual void OnShowing(object data)
+	{
+
+	}
+
+	#endregion
+}
